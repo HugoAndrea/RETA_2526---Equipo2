79626121e4efb06617e5ac3c579932537372b43a
brivero-hub <briverou01@educantabria.es>
2026-05-06 18:12:01 +0200

    BD prueba actualizada
    Se ha avanzado con la creación de la base de datos

--- Tests/BD prueba.sql
+++ Tests/BD prueba.sql
@@ -1,103 +1,93 @@
 create database if not exists inventario;
-create table perfil(
-	id int primary key auto_increment null,
-	tipo enum('administrador','profesor') not null
-);
-
-create table administrador(
-	id int primary key auto_increment not null,
-	nombre varchar(30) not null,
-	contrasenia varchar(50) not null
-);
-
-create table profesor(
-	id int primary key auto_increment not null,
-	nombre varchar(30) not null,
-	contrasenia varchar(50) not null
+-- CHECK (fecha <= CURRENT_DATE)
+-- CHECK (numBalda > 0)
+create table usuario(
+	idUsuario int primary key auto_increment not null,
+    nombre varchar(50) not null,
+    contrasenia varchar(25) not null,
+    rol enum('administrador','profesor')
 );
 
 create table vistas(
 	numVista int primary key auto_increment not null,
-    nombre varchar(30) not null
+    fecha date not null,
+    idUsuario int,
+    foreign key(idUsuario) references usuario(idUsuario)
 );
 
-create table taller(
-	id int primary key auto_increment not null,
-	nombre varchar(30) not null
-);
 
 create table informe(
-	numero int primary key auto_increment not null,
-    tipo enum('inventario','por categoria','localizacion') not null
+	numInforme int primary key auto_increment not null,
+    fecha date not null,
+    descripcion varchar(100) not null,
+    idUsuario int,
+	foreign key(idUsuario) references usuario(idUsuario),
+    CHECK (fecha <= CURRENT_DATE)
 );
+
 
 create table categoria(
-	id int primary key auto_increment not null,
-    tipo enum('PC´s practicas','componentes hardware','equipos de red','cableado estructurado','herramientas','material fungible') not null
+	idCategoria int primary key auto_increment not null,
+    nombre varchar(50) not null
 );
 
-create table PCsPracticas(
-	id int primary key auto_increment not null,
-    nombre varchar(30) not null,
-    descripcion varchar(100) not null,
-    estado boolean not null,
-    cantidad int not null,
-    codigoBalda varchar(30) not null,
-    fechaAlta datetime not null,
-    observaciones varchar(100)
+create table armario(
+idArmario int primary key auto_increment not null,
+nombre varchar(50) not null
 );
 
-create table componentesHardware(
-	id int primary key auto_increment not null,
-    nombre varchar(30) not null,
-    descripcion varchar(100) not null,
-    estado boolean not null,
-    cantidad int not null,
-    codigoBalda varchar(30) not null,
-    fechaAlta datetime not null,
-    observaciones varchar(100)
+create table balda(
+idBalda int primary key auto_increment not null,
+numBalda int not null,
+idArmario int,
+foreign key(idArmario) references armario(idArmario),
+CHECK (numBalda > 0) -- habrá que mirar cuantas baldas hay
 );
 
-create table equiposRed(
-	id int primary key auto_increment not null,
-    nombre varchar(30) not null,
-    descripcion varchar(100) not null,
-    estado boolean not null,
-    cantidad int not null,
-    codigoBalda varchar(30) not null,
-    fechaAlta datetime not null,
-    observaciones varchar(100)
+create table ubicacion(
+idUbicacion int primary key auto_increment not null,
+posicion varchar(25) not null,
+idBalda int,
+foreign key(idBalda) references balda(idBalda)
 );
 
-create table cableadoEstructurado(
-	id int primary key auto_increment not null,
-    nombre varchar(30) not null,
-    descripcion varchar(100) not null,
-    estado boolean not null,
-    cantidad int not null,
-    codigoBalda varchar(30) not null,
-    fechaAlta datetime not null,
-    observaciones varchar(100)
+create table objetoInventario(
+idObjetoInventario int primary key auto_increment not null,
+nombre varchar(30) not null,
+idUbicacion int,
+idCategoria int,
+foreign key (idUbicacion) references ubicacion(idUbicacion),
+foreign key (idCategoria) references categoria(idCategoria)
 );
 
-create table herramientas(
-	id int primary key auto_increment not null,
-    nombre varchar(30) not null,
-    descripcion varchar(100) not null,
-    estado boolean not null,
-    cantidad int not null,
-    codigoBalda varchar(30) not null,
-    fechaAlta datetime not null,
-    observaciones varchar(100)
+create table estado(
+idEstado int primary key auto_increment not null,
+nombre varchar(30) not null
 );
 
-create table materialFungible(
-	id int primary key auto_increment not null,
-    nombre varchar(30) not null,
-    descripcion varchar(100) not null,
-    estado boolean not null,
-    cantidad int not null,
-    codigoBalda varchar(30) not null,
-    fechaAlta datetime not null,
-    observaciones varchar(100)
+create table historialEstado(
+idHistorialEstado int primary key auto_increment not null,
+fecha date not null,
+idObjetoInventario int,
+idEstado int,
+idUsuario int,
+foreign key(idObjetoInventario) references objetoInventario(idObjetoInventario),
+foreign key(idEstado) references estado(idEstado),
+foreign key(idUsuario) references usuario(idUsuario),
+CHECK (fecha <= CURRENT_DATE)
 );
+
+
+
+create table movimiento(
+idMovimiento int primary key auto_increment not null,
+tipo enum('entrada','salida','prestamo','devolucion') not null,
+cantidad int not null,
+fecha date not null,
+idUsuario int,
+idObjetoInventario int,
+foreign key(idObjetoInventario) references objetoInventario(idObjetoInventario),
+foreign key(idUsuario) references usuario(idUsuario),
+CHECK (fecha <= CURRENT_DATE)
+);
+
