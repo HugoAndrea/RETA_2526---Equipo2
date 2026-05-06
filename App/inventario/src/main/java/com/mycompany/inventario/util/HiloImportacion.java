0c70e0ac33de22bf991a9f0f7eee50681d05f1fa
GlebNesterov <gnesterov01@educantabria.es>
2026-05-06 17:10:44 +0200

    esctructura principal de java app
    paquete añadido, "uitl" utiles que usare cundo necesite exportar o importar algo

---
+++ App/inventario/src/main/java/com/mycompany/inventario/util/HiloImportacion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inventario.util;
+
+/**
+ *
+ * @author DAW104
+ * hace la importacion en segundo plano para que la ventana no se congele
+ */
+public class HiloImportacion {
+    
+}
