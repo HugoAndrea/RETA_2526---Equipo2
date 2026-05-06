f8d1b678c1a35fe02b2da04d63475ab5c41d2e2e
GlebNesterov <gnesterov01@educantabria.es>
2026-05-06 17:34:13 +0200

    clase AccesoBase actualizada
    Metodos añadidos:
    - cargarArchivo "falta el archivo prop"
    - abrirConexion
    - getInstance
    - getConn
    - cerrarConexion

--- App/inventario/src/main/java/com/mycompany/inventario/db/AccesoBase.java
+++ App/inventario/src/main/java/com/mycompany/inventario/db/AccesoBase.java
@@ -4,6 +4,13 @@
  */
 package com.mycompany.inventario.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  *
  * @author DAW104
@@ -13,4 +20,74 @@ package com.mycompany.inventario.db;
  */
 public class AccesoBase {
     
+    //todavia no tengo el archivo, pero en cuanto lo tenga lo cargo
+    Properties prop = new Properties();
+    private String url;
+    private String clave;
+    private String usuario;
+    
+    public void cargarArchivo(){
+        try(FileInputStream file = new FileInputStream("nombre del archivo")){
+            prop.load(file);
+            
+            this.url = prop.getProperty("db.url");
+            this.usuario = prop.getProperty("db.usuario");
+            this.clave = prop.getProperty("db.clave");
+            
+            System.out.println("Configuracion Cargada");
+        } catch (IOException e) {
+            System.out.println("Error al cargar el archivo con configuraciones");
+        }
+    }
+    
+    private Connection conn;
+    
+    private void abrirConexion(){
+        try{
+            prop.setProperty("url", this.url);
+            prop.setProperty("usuario", this.usuario);
+            prop.setProperty("clave ", this.clave);
+            prop.setProperty("useSSL", "false");
+            prop.setProperty("serverTimezone", "Europe/Madrid");
+            
+            conn = DriverManager.getConnection(this.url, prop);
+            
+        } catch (SQLException e) {
+            System.out.println("Error al abrir la conexion");
+            conn = null;
+        }
+    }
+    
+    private AccesoBase(){
+        abrirConexion();
+    }
+    
+    private static class AccesoBaseDatosHolder{
+        private static final AccesoBase INSTANCE = new AccesoBase();
+    }
+    
+    public Connection getConn(){
+        try{
+            if (conn == null && !conn.isClosed()) {
+                abrirConexion();
+            }
+        }catch(SQLException e){
+            System.out.println("Error añ comprobar el estado de la conexión");
+        }
+        return conn;
+    }
+    
+    public boolean cerrarConexion(){
+        if (conn == null) {
+            return true;
+        }
+        try{
+            conn.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexion");
+            System.out.println("Mensaje: " + e.getMessage());
+            return false;
+        }
+    }
 }
